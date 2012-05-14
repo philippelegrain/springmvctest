0c371e961f50389415c9a5cf7527ce3cdae16266
Philippe Legrain <pl@redstorm.be>
2012-05-15 00:03:18 +0200

    added controller

---
+++ test/net/mbs/web/HelloControllerTests.java
@@ -0,0 +1,17 @@
+package net.mbs.web;
+
+
+import org.springframework.web.servlet.ModelAndView;
+
+import net.mbs.web.HelloController;
+
+import junit.framework.TestCase;
+
+public class HelloControllerTests extends TestCase {
+
+    public void testHandleRequestView() throws Exception{		
+        HelloController controller = new HelloController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);		
+        assertEquals("hello.jsp", modelAndView.getViewName());
+    }
+}
